bf16e7801fcbe588a281e5ed1d130b642f58ee2c
prassamin <prassamin@gmail.com>
2024-11-10 18:52:40 +0600

    v2024.11.2 - Update packging methods

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 [![PRAS CLI](https://raw.githubusercontent.com/PRASSamin/pras-cli/0c737e49903d148fd92004ee04a55b7334a4f29c/pras/icons/prascli.svg)](https://prascli.onrender.com)
 
-[![PyPI version](https://img.shields.io/pypi/v/pras-cli-nux.svg)](https://pypi.org/project/pras-cli-nux/) [![License](https://img.shields.io/pypi/l/pras-cli-nux.svg)](https://pypi.org/project/pras-cli-nux/)
+[![PyPI version](https://img.shields.io/github/v/pras-cli-nux.svg)](https://pypi.org/project/pras-cli-nux/) [![License](https://img.shields.io/pypi/l/pras-cli-nux.svg)](https://pypi.org/project/pras-cli-nux/)
 
 **PRAS CLI** is a powerful, cross-platform command-line tool designed to simplify and speed up workflows for developers and tech-savvy users alike. With a suite of essential features and tools, **PRAS CLI** enhances productivity by streamlining everyday tasks, all in a single, easy-to-use package. Built for versatility and efficiency, **PRAS CLI** is your go-to solution for a seamless command-line experience.
 
--- pras/__init__.py
+++ pras/__init__.py
@@ -1,16 +1,20 @@
-__version__ = "2024.11.2"
-CLI_NAME = "PRAS CLI"
-CLI_AUTHOR = "PRAS Samin"
-CLI_AUTHOR_EMAIL = "prassamin@gmail.com"
-CLI_AUTHOR_LINK = "https://github.com/PRASSamin"
-CLI_GITHUB_LINK = "https://github.com/PRASSamin/pras-cli"
-CLI_URL = "https://cli.pras.me"
-CLI_PURPLE = "\033[1;38;5;93m" 
-CLI_DESCRIPTION = """PRAS CLI is a powerful, cross-platform command-line tool designed to simplify and speed up workflows for developers and tech-savvy users alike. With a suite of essential features and tools, PRAS CLI enhances productivity by streamlining everyday tasks, all in a single, easy-to-use package. Built for versatility and efficiency, PRAS CLI is your go-to solution for a seamless command-line experience."""
-CLI_CREDIT = f'''"""
-==========================
-PRAS CLI v{__version__}
-GitHub: {CLI_AUTHOR_LINK}
-==========================
-"""
-'''
+import json
+
+with open("manifest.json") as f:
+    manifest = json.load(f)
+    __version__ = manifest["version"]
+    CLI_NAME = manifest["name"]
+    CLI_AUTHOR = manifest["author"]
+    CLI_AUTHOR_EMAIL = manifest["author_email"]
+    CLI_AUTHOR_LINK = manifest["author_url"]
+    CLI_GITHUB_LINK = manifest["repository"]
+    CLI_URL = manifest["homepage"]
+    CLI_PURPLE = "\033[1;38;5;93m" 
+    CLI_DESCRIPTION = manifest["description"]
+    CLI_CREDIT = f'''"""
+    ==========================
+    PRAS CLI v{__version__}
+    GitHub: {CLI_AUTHOR_LINK}
+    ==========================
+    """
+    '''
